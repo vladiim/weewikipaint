bef01c00a349fdc22c6fb9e0c9d87426d59f1865
vladiim <vladiim@yahoo.com.au>
2012-11-29 07:54:30 +1100

    about to integrate

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -1,9 +1,8 @@
 "use strict";
 
 var server = require("./server.js");
-var assert = require("assert");
 
 exports.testNothing = function(test) {
-  assert.equal(3, server.number(), 'number');
+  test.equal(3, server.number(), 'number');
   test.done();
 };
