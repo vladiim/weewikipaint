e2dba27f0c0d5008dd95f7648ca090e49927a027
vladiim <vladiim@yahoo.com.au>
2012-12-03 08:14:44 +1100

    First test working

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -10,10 +10,20 @@ exports.tearDown = function(done) {
   });
 };
 
-exports.testServerRespondsToGet = function(test) {
+exports.test_ServerRespondsToGet = function(test) {
   server.start();
 
   http.get("http://localhost:8080", function(response) {
   	test.done();
+  });
+};
+
+exports.test_serverReturnsHelloWorld = function(test) {
+  server.start();
+
+  var request = http.get("http://localhost:8080");
+  request.on("response", function(response) {
+  	test.equals(200, response.statusCode, "status code");
+    test.done();
   });
 };
